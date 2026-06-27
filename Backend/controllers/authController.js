01d6ec345aa566b235cbada0b98b45f002bec299
M-Kay7634 <manishlloyd2807@gmail.com>
2026-06-27 12:59:22 +0530

    Initial project setup with authentication module

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,79 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register User
+const registerUser = async (req, res) => {
+
+    console.log("===========");
+    console.log("BODY:", req.body);
+    console.log("===========");
+    
+  try {
+    const { name, email, password, role } = req.body;
+
+    // Check required fields
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Please fill all required fields",
+      });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: role || "Analyst",
+    });
+
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error("ERROR MESSAGE:", error.message);
+    console.error(error);
+
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
+}
+};
+
+module.exports = {
+  registerUser,
+};
